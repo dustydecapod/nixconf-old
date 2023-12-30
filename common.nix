4f858a0f0176edf61f7ce4c91256c2ded83b95b8
Ber Zoidberg <ber.zoidberg@gmail.com>
2023-12-30 00:17:26 -0800

    modularize a bit

---
+++ common.nix
@@ -0,0 +1,91 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, lib, pkgs, ... }:
+
+with lib;
+
+{
+  imports = [
+    ./users/berz.nix
+  ];
+
+  options = {
+    hostname = mkOption {
+      type = types.str;
+      default = "unnamed";
+      example = "chewy";
+      description = "hostname";
+    };
+  };
+
+  config = {
+    # Experimental Features
+    nix.settings = {
+      experimental-features = ["nix-command" "flakes"];
+    };
+
+    security.polkit.enable = true;
+
+    # Bootloader.
+    boot.loader.systemd-boot.enable = true;
+    boot.loader.efi.canTouchEfiVariables = true;
+
+    networking.hostName = "${config.hostname}";
+    networking.networkmanager.enable = true;
+
+    # Set your time zone.
+    time.timeZone = "America/Los_Angeles";
+
+    # Select internationalisation properties.
+    i18n.defaultLocale = "en_US.UTF-8";
+
+    i18n.extraLocaleSettings = {
+      LC_ADDRESS = "en_US.UTF-8";
+      LC_IDENTIFICATION = "en_US.UTF-8";
+      LC_MEASUREMENT = "en_US.UTF-8";
+      LC_MONETARY = "en_US.UTF-8";
+      LC_NAME = "en_US.UTF-8";
+      LC_NUMERIC = "en_US.UTF-8";
+      LC_PAPER = "en_US.UTF-8";
+      LC_TELEPHONE = "en_US.UTF-8";
+      LC_TIME = "en_US.UTF-8";
+    };
+
+    # Greetd!
+    services.greetd = {
+      enable = true;
+      settings = {
+        default_session.command = ''
+        ${pkgs.greetd.tuigreet}/bin/tuigreet \
+          --time \
+          --asterisks \
+          --user-menu \
+          --cmd bash
+        '';
+      };
+    };
+
+    # Allow unfree packages
+    nixpkgs.config.allowUnfree = true;
+
+    hardware.opengl = {
+      enable = true;
+      driSupport = true;
+      driSupport32Bit = true;
+    };
+
+    # List packages installed in system profile. To search, run:
+    # $ nix search wget
+    environment.systemPackages = with pkgs; [
+      vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+      wget
+      git
+      curl
+    ];
+
+    programs.mtr.enable = true;
+    system.stateVersion = "23.11"; # Did you read the comment?
+  };
+}
--- flake.nix
+++ flake.nix
@@ -1,41 +1,31 @@
 {
-  description = "Ryan's NixOS Flake";
+  description = "Zoidbb's NixOS Flake";
 
   inputs = {
-    Hyprland.url = "https://flakehub.com/f/hyprwm/Hyprland/0.33.1.tar.gz";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
-
-    home-manager = {
-      url = "github:nix-community/home-manager/release-23.11";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    fh.url = "https://flakehub.com/f/DeterminateSystems/fh/*.tar.gz";
   };
 
-  outputs = { self, nixpkgs, fh, Hyprland, home-manager, ... }@inputs: {
+  outputs = { self, nixpkgs, ... }@inputs: {
     nixosConfigurations = {
       "chewy" = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
-
-        modules = [
-
-          # Flakehub
-          {
-            environment.systemPackages = [
-              fh.packages.x86_64-linux.default
-            ];
+        modules = [       
+          ./hardware/chewy.nix
+          ./common.nix {
+            hostname = "chewy";
           }
+          ./xserver.nix
+        ];
+      };
 
-          # Home-Manager
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.berz = import ./users/berz.nix;
+      "mort" = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [       
+          ./hardware/mort.nix
+          ./common.nix {
+            hostname = "mort";
           }
-            
-          ./configuration.nix
+          ./xserver.nix
         ];
       };
     };
